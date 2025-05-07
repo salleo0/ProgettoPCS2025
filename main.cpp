37b1ca32b313d1e4ffc8c778f4a959d7ab115b64
Adriano <s308388@studenti.polito.it>
2025-05-07 15:03:13 +0200

    Aggiunta cmake

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <sstream>
+#include "FileProva.hpp"
 
 int main(int argc, char *argv[])
 {
-	unsigned int p;
+	/*unsigned int p;
 	unsigned int q;
 	unsigned int b;
 	unsigned int c;
@@ -16,7 +17,10 @@ int main(int argc, char *argv[])
 	
 	convert >> p >> q >> b >> c;
 	if (argc == 7)
-		convert >> id_vertice_1 >> id_vertice_2;
-		
+		convert >> id_vertice_1 >> id_vertice_2;*/
+	std::cout<<"main eseguito"<<std::endl;
+	poliedro poliedro_1;
+	poliedro_1.numero_lati = 2;
+	cout<<poliedro_1.numero_lati;
 	return 0;
 }
---
+++ src/FileProva.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include"Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+struct poliedro
+{
+	unsigned int numero_lati;
+	
+};
